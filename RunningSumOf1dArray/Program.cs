35cca8f69747fbb6509f59e4c11bea4800c98d0e
ChadHsu <cj272842@gmail.com>
2020-09-15 22:57:31 +0800

    first leetcode practice

---
+++ RunningSumOf1dArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RunningSumOf1dArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4 };
+            int[] output = RunningSum(nums);
+            foreach (int x in output)
+            {
+                Console.WriteLine(x);
+            };
+            Console.ReadLine();
+        }
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] output = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    output[i] += nums[j];
+                }
+            };
+
+            return output;
+        }
+    }
+}
---
+++ leetcode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4 };
+            int[] output = RunningSum(nums);
+            foreach (int x in output)
+            {
+                Console.WriteLine(x);
+            };
+            Console.ReadLine();
+        }
+    }
+}
